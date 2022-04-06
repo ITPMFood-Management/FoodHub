7a26887a44c2f223b4ff3cc0d554a8b22ecb040d
thisunsamarasekara <thisunsamarasekara@gmail.com>
2022-04-06 08:21:52 +0530

    create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,12 @@
-import React, { Component } from 'react'
+import React from "react";
+import AddFood from './components/AddFood';
 
-import axios from 'axios';
-
-export default class App extends Component {
-   constructor(props){
-     super(props);
-
-     this.state={
-        foods:[]
-     };
-   }
-    
-   componentDidMount(){
-     this.retrievePosts();
-   }
-    
-   retrievePosts(){
-     axios.get("http://localhost:8000/food").then(res =>{
-       if(res.data.success){
-         this.setState({
-           foods:res.data.existingPosts
-         });
-         console.log(this.state.foods)
-       }
-     })
-   }
-   
-  
-
-   
-
-  render() {
-    return (
-      <div>
-        {this.state.foods.map(foods =>(
+function App(){
+    return(
         <div>
-          <p>{foods.Foodname}</p>
-          <p>{foods.Itemprice}</p>
-          <p>{foods.Foodcategory}</p>
-          <p>{foods.Foodstatus}</p>
+     <AddFood/>   
         </div>
-        )
-          )}
-      </div>
-    )
-  }
+    );
 }
 
+export default App;

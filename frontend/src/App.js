e15dc32f56d200837813a503adb620c795c29e01
thisunsamarasekara <thisunsamarasekara@gmail.com>
2022-04-10 19:51:36 +0530

    update2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
-import AddFood from './components/AddFood';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import Addcustomer from "./components/customerM/addcustomer";
+import Customerlist from "./components/customerM/customerlist";
+import Editcustomer from "./components/customerM/editcustomer";
+import Foodhub from "./components/foodhub";
+import SignInForm from "./components/pages/signinform";
+import SignUpForm from "./components/pages/signupform";
+import Footer from "./footer";
+import Home from "./Home";
 
-function App(){
-    return(
-        <div>
-     <AddFood/>   
-        </div>
+import User from "./user";
+
+
+
+const App = () => {
+    return (  
+        <div className="App">
+      
+      <Routes>
+        
+        <Route path="/" element={<Foodhub/>} />
+        <Route path="/user" element={<User/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/addcustomer" element={<Addcustomer/>} />
+        <Route path="/signin" element={<SignInForm/>} />
+        <Route path="/signup" element={<SignUpForm/>}/>
+        <Route path="/customerlist" element={<Customerlist/>}/>
+        <Route path="/Editcustomer" element={<Editcustomer/>}/>
+  
+       
+ 
+      </Routes>
+      <Footer/>
+    </div>
     );
 }
-
+ 
 export default App;

6aad15bb489da4cc27a32833941772516defdfe0
thisunsamarasekara <thisunsamarasekara@gmail.com>
2022-04-13 17:51:08 +0530

    edit add food 04/13

--- frontend/src/components/FoodM/AddFood.js
+++ frontend/src/components/FoodM/AddFood.js
@@ -1,7 +1,7 @@
 import React,{useState} from "react"
 import './AddFood.css';
 import axios from "axios";
-/*import { ToastContainer, toast } from "react-toastify";*/
+import { ToastContainer, toast } from "react-toastify";
 
 
 

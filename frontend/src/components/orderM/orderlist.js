5e0d0a58c43a9848fef8981b90e52ed34aa68822
Seshmi <it20036898@my.sliit.lk>
2022-04-16 01:11:11 +0530

    Edited View orders

--- frontend/src/components/orderM/orderlist.js
+++ frontend/src/components/orderM/orderlist.js
@@ -56,6 +56,9 @@ export default function OrderList(){
   </div>
 </header>
 
+<br></br>
+<br></br>
+
       <div className="usersDisplay">
      
    
@@ -72,9 +75,7 @@ export default function OrderList(){
                                        <th scope="col" class="p-4">
  
                                        </th>
-                                       <th scope="col" class="p-4 text-left text-xs font-medium text-gray-500 uppercase">
- 
-                                       </th>
+                                       
                                        <th scope="col" class="p-4 text-left text-xs font-medium text-gray-500 uppercase">
                                            Customer Name
                                        </th>
